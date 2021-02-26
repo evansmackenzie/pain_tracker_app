19d1fd4e64c71fae39205c32839c2c656e096f55
mackenzie <evans.mackenzie@gmail.com>
2021-02-26 14:49:45 -0500

    adds CRUD actions for Post model

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,42 @@
 class Api::PostsController < ApplicationController
+    
+  before_action :authenticate_user, except: [:create]
+
+  def index
+    @posts = current_user.posts
+    render "index.json.jb"
+  end
+
+  def show
+    @post = current_user.posts.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def create
+    @post = Post.create({
+      user_id: current_user.id,
+      body: params[:body]
+    })
+    if @post.save
+      render "show.json.jb"
+    else
+      render json: {message: @post.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @post = current_user.posts.find_by(id: params[:id])
+    @post.body = params[:body] || @post.name
+    if @post.save
+      render "show.json.jb"
+    else
+      render json: {message: @post.errors.full_messages}, status: :unprocessable_entity
+    end 
+  end
+
+  def destroy
+    @post = current_user.posts.find_by(id: params[:id])
+    @post.destroy
+    render json: {message: "Your product was destroyed!"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,12 @@ Rails.application.routes.draw do
     get "/users/:id" => "users#show"
     patch "/users/:id" => "users#update"
     delete "/users/:id" => "users#destroy"
+
+    get "/posts" => "posts#index"
+    get "/posts/:id" => "posts#show"
+    post "/posts" => "posts#create"
+    patch "/posts/:id" => "posts#update"
+    delete "/posts/:id" => "posts#destroy"
   end
 
 end
